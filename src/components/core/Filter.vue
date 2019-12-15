b7a8371f197301031d00c141de568546698c3cf7
pkazmierczakk <pkazmierczak@sigma.ug.edu.pl>
2019-12-15 20:16:51 +0100

    #3: Add login page

--- src/components/core/Filter.vue
+++ src/components/core/Filter.vue
@@ -1,65 +1,65 @@
 <template v-slot:activator="{ on }">
-  <v-menu
-    :close-on-content-click="false"
-    bottom
-    left
-    min-width="300"
-    max-width="300"
-    nudge-left="12"
-    offset-x
-    transition="slide-y-transition"
-  >
-    <v-btn
-      slot="activator"
-      class="elevation-0"
-      color="grey"
-      dark
-      fab
-      fixed
-      style="top: 96px;"
-      top
+    <v-menu
+            :close-on-content-click="false"
+            bottom
+            left
+            min-width="300"
+            max-width="300"
+            nudge-left="12"
+            offset-x
+            transition="slide-y-transition"
     >
-      <v-icon>mdi-settings</v-icon>
-    </v-btn>
-    <v-card>
-      <v-container grid-list-xl>
-        <v-layout wrap>
-          <v-flex xs12>
-            <div class="text-xs-center body-2 text-uppercase sidebar-filter">Sidebar Filters</div>
+        <v-btn
+                slot="activator"
+                class="elevation-0"
+                color="grey"
+                dark
+                fab
+                fixed
+                style="top: 96px;"
+                top
+        >
+            <v-icon>mdi-settings</v-icon>
+        </v-btn>
+        <v-card>
+            <v-container grid-list-xl>
+                <v-layout wrap>
+                    <v-flex xs12>
+                        <div class="text-xs-center body-2 text-uppercase sidebar-filter">Sidebar Filters</div>
 
-            <v-layout justify-center>
-              <v-avatar
-                v-for="c in colors"
-                :key="c"
-                :class="[c === color ? 'color-active color-' + c: 'color-' + c]"
-                size="23"
+                        <v-layout justify-center>
+                            <v-avatar
+                                    v-for="c in colors"
+                                    :key="c"
+                                    :class="[c === color ? 'color-active color-' + c: 'color-' + c]"
+                                    size="23"
 
-                @click="setColor(c)"
-              />
-            </v-layout>
-            <v-divider class="mt-3"/>
-          </v-flex>
-          <v-flex
-            xs12
-          >
-            <div class="text-xs-center body-2 text-uppercase sidebar-filter">Images</div>
-          </v-flex>
-          <v-flex
-            v-for="img in images"
-            :key="img"
-            xs3
-          >
-            <v-img
-              :class="[image === img ? 'image-active' : '']"
-              :src="img"
-              height="120"
-              @click.native="setImage(img)"
-            />
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </v-menu>
+                                    @click="setColor(c)"
+                            />
+                        </v-layout>
+                        <v-divider class="mt-3"/>
+                    </v-flex>
+                    <v-flex
+                            xs12
+                    >
+                        <div class="text-xs-center body-2 text-uppercase sidebar-filter">Images</div>
+                    </v-flex>
+                    <v-flex
+                            v-for="img in images"
+                            :key="img"
+                            xs3
+                    >
+                        <v-img
+                                :class="[image === img ? 'image-active' : '']"
+                                :src="img"
+                                height="120"
+                                @click.native="setImage(img)"
+                        />
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </v-menu>
 </template>
 
 <script>
@@ -97,8 +97,8 @@ export default {
 </script>
 
 <style lang="scss">
-  .v-avatar,
-  .v-responsive {
-    cursor: pointer;
-  }
+    .v-avatar,
+    .v-responsive {
+        cursor: pointer;
+    }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-app id="inspire">
+        <v-content>
+            <v-container
+                    class="fill-height"
+                    fluid
+            >
+                <v-row
+                        align="center"
+                        justify="center"
+                >
+                    <v-col
+                            cols="12"
+                            sm="8"
+                            md="4"
+                    >
+                        <v-card class="elevation-12">
+                            <v-toolbar
+                                    color="primary"
+                                    dark
+                                    flat
+                            >
+                                <v-toolbar-title>Login form</v-toolbar-title>
+                                <v-spacer/>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn
+                                                :href="source"
+                                                icon
+                                                large
+                                                target="_blank"
+                                                v-on="on"
+                                        >
+                                            <v-icon>mdi-code-tags</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>Source</span>
+                                </v-tooltip>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field
+                                            v-model="input.username"
+                                            label="Login"
+                                            name="username"
+                                            prepend-icon="person"
+                                            type="text"
+                                    />
+
+                                    <v-text-field
+                                            v-model="input.password"
+                                            id="password"
+                                            label="Password"
+                                            name="password"
+                                            prepend-icon="lock"
+                                            type="password"
+                                    />
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer/>
+                                <v-btn color="primary" v-on:click="login()">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    username: '',
+    password: '',
+    data () {
+        return {
+            input: {
+                username: '',
+                password: ''
+            }
+        };
+    },
+    methods: {
+        login () {
+            const username = this.input.username;
+            const password = this.input.password;
+            if (username === 'admin' && password === 'admin') {
+                this.$store.getters.isLogged = true;
+                this.$router.replace({ name: 'dashboard-index' });
+            } else {
+                return;
+            }
+        }
+    },
+    props: {
+        source: String
+    }
+};
+</script>
